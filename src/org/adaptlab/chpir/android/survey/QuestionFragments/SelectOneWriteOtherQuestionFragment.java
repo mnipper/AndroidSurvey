d3a5e433d337229bae1b82f7ba430a2130fc9199
mn127 <mjn4406@gmail.com>
2013-10-14 22:00:46 -0400

    Only enable other EditText when other radio selected

--- src/org/adaptlab/chpir/android/survey/QuestionFragments/SelectOneWriteOtherQuestionFragment.java
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/SelectOneWriteOtherQuestionFragment.java
@@ -1,9 +1,12 @@
 package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import org.adaptlab.chpir.android.survey.R;
+
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class SelectOneWriteOtherQuestionFragment extends
         SelectOneQuestionFragment {
@@ -11,12 +14,20 @@ public class SelectOneWriteOtherQuestionFragment extends
     @Override
     protected void beforeAddViewHook(ViewGroup questionComponent) {
         RadioButton radioButton = new RadioButton(getActivity());
+        final EditText otherText = new EditText(getActivity());
+        
         radioButton.setText(R.string.other_specify);
-        radioButton.setId(getQuestion().options().size());
-        getRadioGroup().addView(radioButton, getQuestion().options().size());
-
-        EditText otherText = new EditText(getActivity());
+        final int otherId = getQuestion().options().size();
+        radioButton.setId(otherId);
+        getRadioGroup().setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                otherText.setEnabled(checkedId == otherId);
+            }
+        });
+        getRadioGroup().addView(radioButton, otherId);
+        
         otherText.setHint(R.string.other_specify_edittext);
+        otherText.setEnabled(false);
         questionComponent.addView(otherText);
     }
 }

84309472e3a766f22bc4cf6b62f2ac7a2b495007
mn127 <mjn4406@gmail.com>
2013-10-17 15:04:09 -0400

    Crush kyles self esteem again

--- src/org/adaptlab/chpir/android/activerecordcloudsync/ActiveRecordCloudSync.java
+++ src/org/adaptlab/chpir/android/activerecordcloudsync/ActiveRecordCloudSync.java
@@ -2,7 +2,6 @@ package org.adaptlab.chpir.android.activerecordcloudsync;
 
 import java.util.ArrayList;
 
-// Kyle is a cool kid
 public class ActiveRecordCloudSync {
     private static ArrayList<Class<?>> mReceiveTables = new ArrayList<Class<?>>();
     private static String mEndPoint;

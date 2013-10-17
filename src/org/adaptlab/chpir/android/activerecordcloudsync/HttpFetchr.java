41f8f692f79612f8a1de34a0c82619232e985d66
mn127 <mjn4406@gmail.com>
2013-10-17 15:46:45 -0400

    Get rid of un-used imports

--- src/org/adaptlab/chpir/android/activerecordcloudsync/HttpFetchr.java
+++ src/org/adaptlab/chpir/android/activerecordcloudsync/HttpFetchr.java
@@ -5,15 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.activeandroid.Model;
-
-import android.net.Uri;
 import android.util.Log;
 
 public class HttpFetchr {

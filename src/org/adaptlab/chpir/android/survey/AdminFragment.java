f9ab5acaf8cca094127c0cc8e2db3c4682338183
mn127 <mjn4406@gmail.com>
2013-11-01 14:03:10 -0400

    Add placeholder hook methods

--- src/org/adaptlab/chpir/android/survey/AdminFragment.java
+++ src/org/adaptlab/chpir/android/survey/AdminFragment.java
@@ -1,7 +1,22 @@
 package org.adaptlab.chpir.android.survey;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AdminFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_instrument, parent, false);
+        return v;
+    }
 }

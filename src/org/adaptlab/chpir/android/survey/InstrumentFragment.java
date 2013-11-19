e3dc4e6f407060bd47e2b21a8c3677b615a47cd0
mn127 <mjn4406@gmail.com>
2013-11-19 17:10:23 -0500

    Improve instrument list UI

--- src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
+++ src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
@@ -32,99 +32,109 @@ import android.widget.TextView;
 public class InstrumentFragment extends ListFragment {
     private final static String TAG = "InstrumentFragment";
 
-    private Instrument mInstrument;
     private List<Instrument> mInstrumentList;
-
-    private Button mStartButton;
-    private Spinner mInstrumentSpinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-        
+
         appInit();
         mInstrumentList = Instrument.getAll();
-        
+
         InstrumentAdapter adapter = new InstrumentAdapter(mInstrumentList);
         setListAdapter(adapter);
         Log.d(TAG, "Instrument list is: " + mInstrumentList);
     }
-    
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_instrument, menu);
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_item_admin:
-                Intent i = new Intent(getActivity(), AdminActivity.class);
-                startActivity(i); 
-            case R.id.menu_item_refresh:
-                mInstrumentList = Instrument.getAll();
-                InstrumentAdapter adapter = new InstrumentAdapter(mInstrumentList);
-                setListAdapter(adapter);
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.menu_item_admin:
+            Intent i = new Intent(getActivity(), AdminActivity.class);
+            startActivity(i);
+        case R.id.menu_item_refresh:
+            mInstrumentList = Instrument.getAll();
+            InstrumentAdapter adapter = new InstrumentAdapter(mInstrumentList);
+            setListAdapter(adapter);
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
-    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((InstrumentAdapter) getListAdapter()).notifyDataSetChanged();
+    }
+
     private class InstrumentAdapter extends ArrayAdapter<Instrument> {
         public InstrumentAdapter(List<Instrument> instruments) {
             super(getActivity(), 0, instruments);
         }
-        
+
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = getActivity().getLayoutInflater()
-                    .inflate(R.layout.list_item_instrument, null);
+                convertView = getActivity().getLayoutInflater().inflate(
+                        R.layout.list_item_instrument, null);
             }
-            
+
             Instrument instrument = getItem(position);
-            
-            TextView titleTextView =
-                    (TextView)convertView.findViewById(R.id.instrument_list_item_titleTextView);
+
+            TextView titleTextView = (TextView) convertView
+                    .findViewById(R.id.instrument_list_item_titleTextView);
             titleTextView.setText(instrument.getTitle());
-            
+
+            TextView surveysCompleteTextView = (TextView) convertView
+                    .findViewById(R.id.instrument_list_item_surveysCompleteTextView);
+            surveysCompleteTextView.setText(instrument.surveys().size() + " "
+                    + getString(R.string.surveys));
+
             return convertView;
-        }    
+        }
     }
-    
+
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Instrument instrument = ((InstrumentAdapter)getListAdapter()).getItem(position);
+        Instrument instrument = ((InstrumentAdapter) getListAdapter())
+                .getItem(position);
         if (instrument == null || instrument.questions().size() == 0) {
             return;
         }
-        
+
         long instrumentId = instrument.getId();
         Intent i = new Intent(getActivity(), SurveyActivity.class);
         i.putExtra(SurveyFragment.EXTRA_INSTRUMENT_ID, instrumentId);
         startActivity(i);
     }
-    
+
     private final void appInit() {
         Log.i(TAG, "Initializing application...");
         DatabaseSeed.seed(getActivity());
-        
+
         AdminSettings.getInstance().setDeviceIdentifier("TestDevice1");
         AdminSettings.getInstance().setSyncInterval(2);
         AdminSettings.getInstance().save();
-        
-        PollService.setPollInterval(AdminSettings.getInstance().getSyncInterval());
 
-        ActiveRecordCloudSync.setEndPoint(AdminSettings.getInstance().getApiUrl());
+        PollService.setPollInterval(AdminSettings.getInstance()
+                .getSyncInterval());
+
+        ActiveRecordCloudSync.setEndPoint(AdminSettings.getInstance()
+                .getApiUrl());
         ActiveRecordCloudSync.addReceiveTable("instruments", Instrument.class);
         ActiveRecordCloudSync.addReceiveTable("questions", Question.class);
         ActiveRecordCloudSync.addReceiveTable("options", Option.class);
 
         ActiveRecordCloudSync.addSendTable("surveys", Survey.class);
         ActiveRecordCloudSync.addSendTable("responses", Response.class);
-        
+
         PollService.setServiceAlarm(getActivity(), true);
     }
 }

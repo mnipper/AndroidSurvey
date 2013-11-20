7d4dbb4bd063ec6056b3c870d21bea062b1dfa9d
mn127 <mjn4406@gmail.com>
2013-11-20 13:03:43 -0500

    Clean up imports

--- src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
+++ src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
@@ -15,18 +15,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class InstrumentFragment extends ListFragment {

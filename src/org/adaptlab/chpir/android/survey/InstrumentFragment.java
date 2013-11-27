528400aabc662f16e5c9c013acdd2154dff96959
mn127 <mjn4406@gmail.com>
2013-11-27 14:18:24 -0500

    Add place for swahili

--- src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
+++ src/org/adaptlab/chpir/android/survey/InstrumentFragment.java
@@ -21,7 +21,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.text.Editable;
 import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;

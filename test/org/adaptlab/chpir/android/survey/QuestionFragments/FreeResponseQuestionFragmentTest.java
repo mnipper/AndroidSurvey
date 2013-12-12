c3d412cb70dd4d558ab570c54673195ec358cb35
Leonard Ng'eno <dranoel.bravo@gmail.com>
2013-12-12 12:08:44 -0500

    slider question fragment tests

--- test/org/adaptlab/chpir/android/survey/QuestionFragments/FreeResponseQuestionFragmentTest.java
+++ test/org/adaptlab/chpir/android/survey/QuestionFragments/FreeResponseQuestionFragmentTest.java
@@ -2,11 +2,11 @@ package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.spy;
 
 import org.adaptlab.chpir.android.survey.QuestionFragmentFactory;
+import org.adaptlab.chpir.android.survey.R;
 import org.adaptlab.chpir.android.survey.SurveyActivity;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,13 +14,11 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.adaptlab.chpir.android.survey.R;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 @RunWith(RobolectricTestRunner.class)
@@ -30,52 +28,57 @@ public class FreeResponseQuestionFragmentTest {
 	private static final long SURVEY_ID = 10L;
 	private FreeResponseQuestionFragment questionFragment;
 	private SurveyActivity activity;
-	
+
 	private void startFragment(FreeResponseQuestionFragment fragment) {
 		FragmentManager fragmentManager = activity.getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add( fragment, null );
-        fragmentTransaction.commit();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+		fragmentTransaction.add(fragment, null);
+		fragmentTransaction.commit();
 	}
-	
+
 	private void setBundleArgs() {
 		Bundle args = new Bundle();
-        args.putLong(QuestionFragmentFactory.EXTRA_QUESTION_ID, REMOTE_ID);
-        args.putLong(QuestionFragmentFactory.EXTRA_SURVEY_ID, SURVEY_ID);
-        questionFragment.setArguments(args);
+		args.putLong(QuestionFragmentFactory.EXTRA_QUESTION_ID, REMOTE_ID);
+		args.putLong(QuestionFragmentFactory.EXTRA_SURVEY_ID, SURVEY_ID);
+		questionFragment.setArguments(args);
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		activity = Robolectric.buildActivity(SurveyActivity.class).create().get();
+		activity = Robolectric.buildActivity(SurveyActivity.class).create()
+				.get();
 		questionFragment = spy(new FreeResponseQuestionFragment());
 		doNothing().when(questionFragment).init();
 		setBundleArgs();
 		startFragment(questionFragment);
 	}
-	
+
 	@Test
 	public void fragmentShouldNotBeNull() throws Exception {
 		assertNotNull(questionFragment);
 	}
-	
+
 	@Test
 	public void questionComponentShoudHaveOneChildView() throws Exception {
 		ViewGroup qComponent = new LinearLayout(Robolectric.application);
 		questionFragment.createQuestionComponent(qComponent);
 		assertEquals(qComponent.getChildCount(), 1);
 	}
-	
+
 	@Test
 	public void questionComponentShouldHaveHintString() throws Exception {
 		ViewGroup qComponent = new LinearLayout(Robolectric.application);
 		questionFragment.createQuestionComponent(qComponent);
-		String hint = Robolectric.application.getResources().getString(R.string.free_response_edittext);
-		String text = questionFragment.getString(R.string.free_response_edittext);
+		String hint = Robolectric.application.getResources().getString(
+				R.string.free_response_edittext);
+		String text = questionFragment
+				.getString(R.string.free_response_edittext);
 		assertEquals(hint, text);
 	}
-	
-	@Test //TODO FIX
+
+	@Test
+	// TODO FIX
 	public void shouldHaveTextChangeListener() throws Exception {
 		ViewGroup qComponent = new LinearLayout(Robolectric.application);
 		questionFragment.createQuestionComponent(qComponent);

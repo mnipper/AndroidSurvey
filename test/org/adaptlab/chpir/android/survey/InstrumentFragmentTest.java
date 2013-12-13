bd31e97357e410150573e67d887f156a2ae5604a
Leonard Ng'eno <dranoel.bravo@gmail.com>
2013-12-13 17:06:59 -0500

    inst frag tests

--- test/org/adaptlab/chpir/android/survey/InstrumentFragmentTest.java
+++ test/org/adaptlab/chpir/android/survey/InstrumentFragmentTest.java
@@ -1,9 +1,11 @@
 package org.adaptlab.chpir.android.survey;
 
+import static org.mockito.Mockito.spy;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
@@ -19,43 +21,44 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(RobolectricTestRunner.class)
+@PrepareForTest({ InstrumentFragment.class })
 public class InstrumentFragmentTest {
-	private InstrumentFragment instFragment;
+	private InstrumentFragment iFragment;
 	private InstrumentActivity activity;
 
 	@Before
 	public void setUp() throws Exception {
+		activity = Robolectric.buildActivity(InstrumentActivity.class).create().get();
+		iFragment = spy(new InstrumentFragment());
 		startFragment();
 	}
 	
 	public void startFragment() {
-		activity = Robolectric.buildActivity(InstrumentActivity.class).create().start().resume().get();
-		instFragment = (InstrumentFragment) activity.createFragment();
 		FragmentManager fragmentManager = activity.getSupportFragmentManager();
 	    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-	    fragmentTransaction.add(instFragment, null );
+	    fragmentTransaction.add(iFragment, null );
 	    fragmentTransaction.commit();
 	}
 	
 	@Test
 	public void shouldHaveOptionsMenuSetToTrue() throws Exception {
-		assertThat(instFragment.hasOptionsMenu(), equalTo(true));
+		assertThat(iFragment.hasOptionsMenu(), equalTo(true));
 	}
 	
 	@Test
 	public void shouldHaveOptionsMenuInflated() throws Exception {
-		assertThat(instFragment.isMenuVisible(), equalTo(true));	//TODO check if this is the correct way to test whether menu has been inflated
+		assertThat(iFragment.isMenuVisible(), equalTo(true));
 	}
 	
-	@Test
+	@Test	//TODO FIX
 	public void shouldDetectAdminMenuItemSelected() throws Exception {
 		MenuItem item = new TestMenuItem() {
 			public int getItemId() {
 			    return R.id.menu_item_admin;
 			  }
 		};
-		instFragment.onOptionsItemSelected(item);
 		ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+		iFragment.onOptionsItemSelected(item);
 		Intent startedIntent = shadowActivity.getNextStartedActivity();
 		ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
 		assertThat(shadowIntent.getComponent().getClassName(), equalTo(AdminActivity.class.getName()));	
@@ -68,8 +71,8 @@ public class InstrumentFragmentTest {
 			    return R.id.menu_item_refresh;
 			  }
 		};
-		instFragment.onOptionsItemSelected(item);
-		assertNotNull(instFragment.getListAdapter());
+		iFragment.onOptionsItemSelected(item);
+		assertNotNull(iFragment.getListAdapter());
 	}
 	
 	//TODO Write tests for remaining methods

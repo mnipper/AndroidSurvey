6b1967c98fd8350fc3fc421b48f9ed03087d9808
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-02-14 15:39:11 -0500

    try to fix onActivityReceived bug

--- src/org/adaptlab/chpir/android/survey/CameraFragment.java
+++ src/org/adaptlab/chpir/android/survey/CameraFragment.java
@@ -22,67 +22,72 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class CameraFragment extends Fragment {
-    private static final String TAG = "CameraFragment";
-    public static final String EXTRA_PHOTO_FILENAME = "org.adaptlab.chpir.android.survey.CameraFragment.filename";
-    private View mProgressIndicator;
+	private static final String TAG = "CameraFragment";
+	public static final String EXTRA_PHOTO_FILENAME = "CameraFragment.filename";
+	private View mProgressIndicator;
 	private Camera mCamera;
-    private SurfaceView mSurfaceView;
-    
-    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
-        public void onShutter() {
-            mProgressIndicator.setVisibility(View.VISIBLE);
-        }
-    };
-    
-    private Camera.PictureCallback mJPEGCallBack = new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            String filename = UUID.randomUUID().toString() + ".jpg";
-            FileOutputStream os = null;
-            boolean success = true;
-            try {
-                os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
-                os.write(data);
-            } catch (Exception e) {
-                Log.e(TAG, "Error writing to file " + filename, e);
-                success = false;
-            } finally {
-                try {
-                    if (os != null)
-                        os.close();
-                } catch (Exception e) {
-                    Log.e(TAG, "Error closing file " + filename, e);
-                    success = false;
-                } 
-            }
-            
-            if (success) {
-                if (success) {
-                    Intent i = new Intent();
-                    i.putExtra(EXTRA_PHOTO_FILENAME, filename);
-                    getActivity().setResult(Activity.RESULT_OK, i);
-                } else {
-                    getActivity().setResult(Activity.RESULT_CANCELED);
-                }
-            }
-            getActivity().finish();
-        }
-    };
-    
+	private SurfaceView mSurfaceView;
+
+	private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+		public void onShutter() {
+			mProgressIndicator.setVisibility(View.VISIBLE);
+		}
+	};
+
+	private Camera.PictureCallback mJPEGCallBack = new Camera.PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			String filename = UUID.randomUUID().toString() + ".jpg";
+			FileOutputStream os = null;
+			boolean success = true;
+			try {
+				os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+				os.write(data);
+				Log.i(TAG, "filename: " + filename);
+			} catch (Exception e) {
+				Log.e(TAG, "Error writing to file " + filename, e);
+				success = false;
+			} finally {
+				try {
+					if (os != null)
+						os.close();
+				} catch (Exception e) {
+					Log.e(TAG, "Error closing file " + filename, e);
+					success = false;
+				} 
+			}
+			if (success) {
+				if (success) {
+					Log.i(TAG, "SUCCESS!!");
+					Log.i(TAG, EXTRA_PHOTO_FILENAME);
+					Intent i = new Intent();
+					i.putExtra(EXTRA_PHOTO_FILENAME, filename);
+					getActivity().setResult(Activity.RESULT_OK, i);
+					Log.i(TAG, "PICTURE SENT");
+				} else {
+					getActivity().setResult(Activity.RESULT_CANCELED);
+				}
+			}
+			getActivity().finish();
+		}
+	};
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_camera, parent, false);
 		mProgressIndicator = v.findViewById(R.id.camera_progressIndicator);
-        mProgressIndicator.setVisibility(View.INVISIBLE);
-        Button takePictureButton = (Button) v.findViewById(R.id.camera_takePictureButton);
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
+		mProgressIndicator.setVisibility(View.INVISIBLE);
+		Button takePictureButton = (Button) v.findViewById(R.id.camera_takePictureButton);
+		takePictureButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				mCamera.takePicture(mShutterCallback, null, mJPEGCallBack);
+				if (mCamera != null) {
+					mCamera.takePicture(mShutterCallback, null, mJPEGCallBack);
+				}
 			}
 		});
-        
-        mSurfaceView = (SurfaceView)v.findViewById(R.id.camera_surfaceView);
-        SurfaceHolder holder = mSurfaceView.getHolder();
+
+		mSurfaceView = (SurfaceView)v.findViewById(R.id.camera_surfaceView);
+		SurfaceHolder holder = mSurfaceView.getHolder();
 		holder.addCallback(new SurfaceHolder.Callback() {
 			public void surfaceCreated(SurfaceHolder holder) {
 				try {
@@ -93,79 +98,89 @@ public class CameraFragment extends Fragment {
 					Log.e(TAG, "Error setting up surface preview display", e);
 				}
 			}
-			
+
 			public void surfaceDestroyed(SurfaceHolder holder) {
 				if (mCamera != null) {
 					mCamera.stopPreview();
 				}
 			}
-			
+
 			public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
 				if (mCamera == null) {
 					return;
 				}
 				Camera.Parameters parameters = mCamera.getParameters();
-                Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
-                parameters.setPreviewSize(s.width, s.height);
-                mCamera.setParameters(parameters);
-                try {
-                	mCamera.startPreview();
-                } catch (Exception e) {
-                	Log.e(TAG, "Unable to start preview",e);
-                }
+				Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
+				parameters.setPreviewSize(s.width, s.height);
+				mCamera.setParameters(parameters);
+				try {
+					mCamera.startPreview();
+				} catch (Exception e) {
+					Log.e(TAG, "Unable to start preview",e);
+				}
 			}	
 		});
-        
+
 		return v;
 	}
-	
+
 	private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
-		final double ASPECT_TOLERANCE = 0.1;
-        double targetRatio = (double) width / height;
-        if (sizes == null) return null;
- 
-        Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-        int targetHeight = height;
- 
-        for (Size size : sizes) {
-            double ratio = (double) size.width / size.height;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
+		/*final double ASPECT_TOLERANCE = 0.1;
+		double targetRatio = (double) width / height;
+		if (sizes == null) return null;
+
+		Size optimalSize = null;
+		double minDiff = Double.MAX_VALUE;
+		int targetHeight = height;
+
+		for (Size size : sizes) {
+			double ratio = (double) size.width / size.height;
+			if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+			if (Math.abs(size.height - targetHeight) < minDiff) {
+				optimalSize = size;
+				minDiff = Math.abs(size.height - targetHeight);
+			}
+		}
+
+		if (optimalSize == null) {
+			minDiff = Double.MAX_VALUE;
+			for (Size size : sizes) {
+				if (Math.abs(size.height - targetHeight) < minDiff) {
+					optimalSize = size;
+					minDiff = Math.abs(size.height - targetHeight);
+				}
+			}
+		}
+		return optimalSize;*/
+		Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
             }
         }
- 
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
-        return optimalSize;
-    }
-	
+        return bestSize;
+	}
+
 	@Override
 	public void onResume() {
 		super.onResume();
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {    
-            mCamera = Camera.open(0);
-        } else {
-            mCamera = Camera.open();
-        } //TODO Check what's the case for a 2-camera device
+			mCamera = Camera.open(0);
+		} else {
+			mCamera = Camera.open();
+		} //TODO Check what's the case for a 2-camera device
 	}
-	
+
 	@Override
-    public void onPause() {
-        super.onPause();
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
-    }
-	
+	public void onPause() {
+		super.onPause();
+		if (mCamera != null) {
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
 }
--- src/org/adaptlab/chpir/android/survey/QuestionFragments/RearPictureQuestionFragment.java
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/RearPictureQuestionFragment.java
@@ -1,51 +1,82 @@
 package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import org.adaptlab.chpir.android.survey.CameraActivity;
+import org.adaptlab.chpir.android.survey.CameraFragment;
 import org.adaptlab.chpir.android.survey.QuestionFragment;
 import org.adaptlab.chpir.android.survey.R;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import com.activeandroid.util.Log;
 
 public class RearPictureQuestionFragment extends QuestionFragment {
+    private static final String TAG = "RearPictureQuestionFragment";
+    private static final int REQUEST_PHOTO = 0;
+
 	private Button mCameraButton;
 	private ImageView mPhoto;
+
+	@Override
+	protected void createQuestionComponent(ViewGroup questionComponent) {
+
+		if (isCameraAvailable()) {
+			mCameraButton = new Button(getActivity());
+			mCameraButton.setText(R.string.enable_camera);
+			mCameraButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Intent i = new Intent(getActivity(), CameraActivity.class);
+					startActivityForResult(i, REQUEST_PHOTO);
+				}
+			});
+			
+			mPhoto = new ImageView(getActivity());
+			int id = getResources().getIdentifier("org.adaptlab.chpir.android.survey:drawable/" + "ic_action_picture", null, null);
+			mPhoto.setImageResource(id);
+
+			questionComponent.addView(mCameraButton);
+			questionComponent.addView(mPhoto);
+		}
+	}
 	
-    @Override
-    protected void createQuestionComponent(ViewGroup questionComponent) {
-        
-    	mCameraButton = new Button(getActivity());
-    	mCameraButton.setText(R.string.enable_camera);
-    	mCameraButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(getActivity(), CameraActivity.class);
-                startActivity(i);
-			}
-		});
-    	
-    	mPhoto = new ImageView(getActivity());
-    	int id = getResources().getIdentifier("org.adaptlab.chpir.android.survey:drawable/" + "ic_action_picture", null, null);
-    	mPhoto.setImageResource(id);
-    	
-    	questionComponent.addView(mCameraButton);
-       	questionComponent.addView(mPhoto);
-    }
+	@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.i(TAG, "RECEIVE PICTURE");
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_PHOTO) {
+            String filename = data.getStringExtra(CameraFragment.EXTRA_PHOTO_FILENAME);
+            if (filename != null) {
+                //Photo p = new Photo(filename);
+                //mCrime.setPhoto(p);
+                //showPhoto();
+            	Log.i(TAG, "FILENAME: " + filename);
+            }
+        }
+	}
 
-    @Override
-    protected String serialize() {
-        return null;
-    }
+	private boolean isCameraAvailable() {
+		PackageManager manager = getActivity().getPackageManager();
+		if (!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+				!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+			return false;
+		} else {
+			return true;
+		}   
+	}
 
-    @Override
-    protected void deserialize(String responseText) {
-  
-    }
-   
+	@Override
+	protected String serialize() {
+		return null;
+	}
+
+	@Override
+	protected void deserialize(String responseText) {
+
+	}
+
 }

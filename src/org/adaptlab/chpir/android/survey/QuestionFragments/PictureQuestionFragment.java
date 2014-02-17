849f10afec69ef30e0f57cf5959e6e284f71bbc1
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-02-17 15:18:04 -0500

    refactor front and rear picture question fragments

---
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/PictureQuestionFragment.java
@@ -0,0 +1,89 @@
+package org.adaptlab.chpir.android.survey.QuestionFragments;
+
+import org.adaptlab.chpir.android.survey.CameraFragment;
+import org.adaptlab.chpir.android.survey.Photo;
+import org.adaptlab.chpir.android.survey.PictureUtils;
+import org.adaptlab.chpir.android.survey.QuestionFragment;
+
+import com.activeandroid.util.Log;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public abstract class PictureQuestionFragment extends QuestionFragment {
+    public static final int REQUEST_PHOTO = 0;
+    private static final String TAG = "PictureQuestionFragment";
+	public CameraFragment mCameraFragment;
+	private ImageView mPhoto;
+	
+	@Override
+	protected abstract void createQuestionComponent(ViewGroup questionComponent);
+	
+	@Override
+	protected void deserialize(String responseText) {
+		// TODO Auto-generated method stub	
+	}
+	
+	protected ImageView getPhoto() {
+		return mPhoto;
+	}
+	
+	protected boolean isCameraAvailable() {
+		PackageManager manager = getActivity().getPackageManager();
+		if (!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+				!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+			return false;
+		} else {
+			return true;
+		}   
+	}
+	
+	@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_PHOTO) {
+            String filename = data.getStringExtra(CameraFragment.EXTRA_PHOTO_FILENAME);
+            if (filename != null) {
+            	Photo picture = new Photo(filename);
+            	showPicture(picture);
+            	Log.i(TAG, "FILENAME: " + filename);
+            }
+        }
+        removeCameraFragment();
+	}
+	
+	private void removeCameraFragment() {
+		FragmentManager fm = getActivity().getSupportFragmentManager();
+		FragmentTransaction transaction = fm.beginTransaction();
+		transaction.remove(mCameraFragment);
+		transaction.commit();
+	}
+	
+	@Override
+	protected String serialize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	protected void setPhoto() {
+		mPhoto = new ImageView(getActivity());
+		int id = getResources().getIdentifier("org.adaptlab.chpir.android.survey:drawable/" + "ic_action_picture", null, null);
+		mPhoto.setImageResource(id);
+	}
+
+	private void showPicture(Photo photo) {
+		BitmapDrawable bitmap = null;
+        if (photo != null) {
+            String path = getActivity().getFileStreamPath(photo.getFilename()).getAbsolutePath();
+            bitmap = PictureUtils.getScaledDrawable(getActivity(), path);
+        }
+        mPhoto.setImageDrawable(bitmap);
+	}
+
+}
--- src/org/adaptlab/chpir/android/survey/QuestionFragments/RearPictureQuestionFragment.java
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/RearPictureQuestionFragment.java
@@ -1,31 +1,17 @@
 package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import org.adaptlab.chpir.android.survey.CameraFragment;
-import org.adaptlab.chpir.android.survey.Photo;
-import org.adaptlab.chpir.android.survey.PictureUtils;
-import org.adaptlab.chpir.android.survey.QuestionFragment;
 import org.adaptlab.chpir.android.survey.R;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import com.activeandroid.util.Log;
-
-public class RearPictureQuestionFragment extends QuestionFragment {
-    private static final String TAG = "RearPictureQuestionFragment";
-    private static final int REQUEST_PHOTO = 0;
+public class RearPictureQuestionFragment extends PictureQuestionFragment {
     private static final int REAR_CAMERA = 0;
 	private Button mCameraButton;
-	private ImageView mPhoto;
-	private CameraFragment mCameraFragment;
 	
 	@Override
 	protected void createQuestionComponent(ViewGroup questionComponent) {
@@ -35,9 +21,6 @@ public class RearPictureQuestionFragment extends QuestionFragment {
 			mCameraButton.setText(R.string.enable_camera);
 			mCameraButton.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
-					Log.i(TAG, "SHOW CAMERA");
-					//getActivity().requestWindowFeature(Window.FEATURE_NO_TITLE);
-			        //getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 					FragmentManager fm = getActivity().getSupportFragmentManager();
 					FragmentTransaction transaction = fm.beginTransaction();
 					mCameraFragment = new CameraFragment();
@@ -47,64 +30,10 @@ public class RearPictureQuestionFragment extends QuestionFragment {
 					transaction.commit();
 				}
 			});
-			
-			mPhoto = new ImageView(getActivity());
-			int id = getResources().getIdentifier("org.adaptlab.chpir.android.survey:drawable/" + "ic_action_picture", null, null);
-			mPhoto.setImageResource(id);
-
+			setPhoto();
 			questionComponent.addView(mCameraButton);
-			questionComponent.addView(mPhoto);
+			questionComponent.addView(getPhoto());
 		}
 	}
 	
-	@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != Activity.RESULT_OK) return;
-        if (requestCode == REQUEST_PHOTO) {
-            String filename = data.getStringExtra(CameraFragment.EXTRA_PHOTO_FILENAME);
-            if (filename != null) {
-            	Photo picture = new Photo(filename);
-            	showPicture(picture);
-            	Log.i(TAG, "FILENAME: " + filename);
-            }
-        }
-        removeCameraFragment();
-	}
-
-	private boolean isCameraAvailable() {
-		PackageManager manager = getActivity().getPackageManager();
-		if (!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
-				!manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
-			return false;
-		} else {
-			return true;
-		}   
-	}
-	
-	private void removeCameraFragment() {
-		FragmentManager fm = getActivity().getSupportFragmentManager();
-		FragmentTransaction transaction = fm.beginTransaction();
-		transaction.remove(mCameraFragment);
-		transaction.commit();
-	}
-	
-	private void showPicture(Photo photo) {
-		BitmapDrawable bitmap = null;
-        if (photo != null) {
-            String path = getActivity().getFileStreamPath(photo.getFilename()).getAbsolutePath();
-            bitmap = PictureUtils.getScaledDrawable(getActivity(), path);
-        }
-        mPhoto.setImageDrawable(bitmap);
-	}
-
-	@Override
-	protected String serialize() {
-		return null;
-	}
-
-	@Override
-	protected void deserialize(String responseText) {
-
-	}
-
 }

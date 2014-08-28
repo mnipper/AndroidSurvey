cac36283ce0029f012cb876b5931068ec84bcc5a
mn127 <mjn4406@gmail.com>
2014-08-28 12:11:32 -0400

    Follow android variable naming convention

--- src/org/adaptlab/chpir/android/survey/AuthUtils.java
+++ src/org/adaptlab/chpir/android/survey/AuthUtils.java
@@ -3,22 +3,22 @@ package org.adaptlab.chpir.android.survey;
 import org.adaptlab.chpir.android.survey.Models.DeviceUser;
 
 public class AuthUtils {
-    private static DeviceUser CURRENT_USER = null;
+    private static DeviceUser sCurrentUser = null;
 
     
     public static boolean isSignedIn() {
-        return CURRENT_USER != null;
+        return sCurrentUser != null;
     }
     
     public static void signOut() {
-        CURRENT_USER = null;
+        sCurrentUser = null;
     }
     
     public static void signIn(DeviceUser deviceUser) {
-        CURRENT_USER = deviceUser;
+        sCurrentUser = deviceUser;
     }
     
     public static DeviceUser getCurrentUser() {
-        return CURRENT_USER;
+        return sCurrentUser;
     }
 }

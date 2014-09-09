f7878a7faf7350b256acd2f6f5c9844adfcb5670
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-09-09 16:52:39 -0400

    check for array bounds

--- src/org/adaptlab/chpir/android/survey/SurveyFragment.java
+++ src/org/adaptlab/chpir/android/survey/SurveyFragment.java
@@ -462,9 +462,11 @@ public class SurveyFragment extends Fragment {
 	}
 
 	private void addQuestionsToSkip(int responseIndex) {
-		Option selectedOption = mQuestion.options().get(responseIndex);
-		for (Question skipQuestion: selectedOption.questionsToSkip()){
-			mQuestionsToSkip.add(skipQuestion);
+		if (responseIndex < mQuestion.options().size()) {
+			Option selectedOption = mQuestion.options().get(responseIndex);
+			for (Question skipQuestion: selectedOption.questionsToSkip()){
+				mQuestionsToSkip.add(skipQuestion);
+			}
 		}
 	}
 

bd8202c118bb86d4ff9996ab88499264b89f50b6
mn127 <mjn4406@gmail.com>
2014-10-27 11:00:17 -0400

    Remove OnSaveResponseListener callback.  See commit 597f13fa84e11349972b03477a3e6de0987b1dba for a more simple implementation that accomplishes the same thing.

--- src/org/adaptlab/chpir/android/survey/SurveyActivity.java
+++ src/org/adaptlab/chpir/android/survey/SurveyActivity.java
@@ -1,24 +1,11 @@
 package org.adaptlab.chpir.android.survey;
 
-import org.adaptlab.chpir.android.survey.Models.Response;
-
 import android.support.v4.app.Fragment;
 
-public class SurveyActivity extends AuthorizedActivity implements QuestionFragment.OnSaveResponseListener {
+public class SurveyActivity extends AuthorizedActivity {
 
 	@Override
 	protected Fragment createFragment() {
 		return new SurveyFragment();
-	}
-
-	@Override
-	public void onResponseSaved() {
-		SurveyFragment surveyFragment = (SurveyFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer); 
-		if (surveyFragment != null) {
-			Response response = surveyFragment.mQuestionFragment.getResponse();
-			if (response != null) {
-				response.setSpecialResponse("");
-			}
-		} 
 	}
 }

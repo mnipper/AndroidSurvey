3fdb182b4cdf444bb25b5bd87a7f0f5f73e6562b
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-11-13 14:43:33 -0500

    make question component invisible for instructions question types

--- src/org/adaptlab/chpir/android/survey/QuestionFragments/InstructionsQuestionFragment.java
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/InstructionsQuestionFragment.java
@@ -2,6 +2,7 @@ package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import org.adaptlab.chpir.android.survey.QuestionFragment;
 
+import android.view.View;
 import android.view.ViewGroup;
 
 /*
@@ -11,15 +12,17 @@ import android.view.ViewGroup;
  */
 public class InstructionsQuestionFragment extends QuestionFragment {
 
-    @Override
-    protected void createQuestionComponent(ViewGroup questionComponent) { }
+	@Override
+	protected void createQuestionComponent(ViewGroup questionComponent) { 
+		questionComponent.setVisibility(View.INVISIBLE);
+	}
 
-    @Override
-    protected String serialize() {
-        return "";
-    }
+	@Override
+	protected String serialize() {
+		return "";
+	}
 
-    @Override
-    protected void deserialize(String responseText) { }
+	@Override
+	protected void deserialize(String responseText) { }
 
 }

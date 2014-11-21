9c826296ab2d24ca070ff5169f50cf411f77c3fa
mn127 <mjn4406@gmail.com>
2014-11-21 16:46:18 -0500

    Add consent form model

---
+++ src/org/adaptlab/chpir/android/survey/Models/ConsentForm.java
@@ -0,0 +1,81 @@
+package org.adaptlab.chpir.android.survey.Models;
+
+import java.util.Date;
+
+import org.adaptlab.chpir.android.activerecordcloudsync.SendModel;
+import org.adaptlab.chpir.android.survey.EncryptUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "ConsentForms")
+public class ConsentForm extends SendModel {
+    private static final String TAG = "ConsentForm";
+    
+    @Column(name = "Name")
+    private String mName;
+    @Column(name = "Email")
+    private String mEmail;
+    @Column(name = "Date")
+    private Date mDate;
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject json = new JSONObject();
+        
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", getName());
+            jsonObject.put("email", getEmail());
+            jsonObject.put("date", getDate());
+
+            json.put("consent_form", jsonObject);
+        } catch (JSONException je) {
+            Log.e(TAG, "JSON exception", je);
+        }
+        
+        return json;
+    }
+
+    @Override
+    public boolean isSent() { return false; }
+
+    @Override
+    public boolean readyToSend() { return true; }
+
+    @Override
+    public boolean isPersistent() { return true; }
+
+    @Override
+    public void setAsSent() {
+        this.delete();
+    }
+    
+    public void setName(String name) {
+        mName = EncryptUtil.encrypt(name);
+    }
+    
+    public void setEmail(String email) {
+        mEmail = EncryptUtil.encrypt(email);
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    
+    private String getName() {
+        return mName;
+    }
+    
+    private String getEmail() {
+        return mEmail;
+    }
+    
+    public Date getDate() {
+        return mDate;
+    }
+}

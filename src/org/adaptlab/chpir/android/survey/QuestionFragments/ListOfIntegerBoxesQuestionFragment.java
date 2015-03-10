0fe4c61c5711413d916811f8efffa472a728402a
mn127 <mjn4406@gmail.com>
2015-03-10 17:10:11 -0400

    Bump version

--- src/org/adaptlab/chpir/android/survey/QuestionFragments/ListOfIntegerBoxesQuestionFragment.java
+++ src/org/adaptlab/chpir/android/survey/QuestionFragments/ListOfIntegerBoxesQuestionFragment.java
@@ -1,15 +1,14 @@
 package org.adaptlab.chpir.android.survey.QuestionFragments;
 
 import android.text.InputType;
-import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class ListOfIntegerBoxesQuestionFragment extends ListOfItemsQuestionFragment {
-
+    
     @Override
-    protected void createQuestionComponent(ViewGroup questionComponent) {
+    protected EditText createEditText() {
         EditText editText = new EditText(getActivity());
         editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
-        createQuestionComponent(questionComponent, editText);
+        return editText;
     }
 }
